7415cbeed11ae4048b43be13d9da1392e1536b0c
YvesHouedande <yveshouedandedocteur@gmail.com>
2024-12-27 14:14:44 +0000

    prod env set with docker

--- backendProject/backend/settings.py
+++ backendProject/backend/settings.py
@@ -15,7 +15,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
